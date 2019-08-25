028c14d7eba82fb9b38027a8c97e81a8688fa5f9
tatsuya-of-INM <is0514vf@ed.ritsumei.ac.jp>
2019-08-25 21:13:17 +0900

    opration sample

---
+++ Assets/Scripts/BulletBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 pos = transform.position;
+        pos.x = 0f; pos.y = 10.5f; pos.z = 5f;
+        transform.position = pos;
+        transform.eulerAngles = new Vector3(0, 0, 0);
+        gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 50);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TargetOperation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetOperation : MonoBehaviour
+{
+
+    private Vector3 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (Input.GetKey(KeyCode.UpArrow))    pos.y = pos.y + 0.5f; 
+        if (Input.GetKey(KeyCode.DownArrow))  pos.y = pos.y - 0.5f;
+        if (Input.GetKey(KeyCode.LeftArrow))  pos.x = pos.x + 0.5f;
+        if (Input.GetKey(KeyCode.RightArrow)) pos.x = pos.x - 0.5f;
+
+        transform.position = pos;
+    }
+}
