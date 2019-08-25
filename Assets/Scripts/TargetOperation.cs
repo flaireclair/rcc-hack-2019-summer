1110e369439950a33406b88b05325be38d9271c4
flaireclair <h.kmntag6.gaburiasu@gmail.com>
2019-08-25 22:56:59 +0900

    create scenes

--- Assets/Scripts/TargetOperation.cs
+++ Assets/Scripts/TargetOperation.cs
@@ -6,21 +6,34 @@ public class TargetOperation : MonoBehaviour
 {
 
     private Vector3 pos;
+    private Transform tran;
+    private Vector2 max;
+    private Vector2 min;
 
     // Start is called before the first frame update
     void Start()
     {
-        pos = transform.position;
+        tran = transform;
+        pos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        max = new Vector2(30, 61);
+        min = new Vector2(-30, 1);
     }
 
     // Update is called once per frame
     void Update()
-    {   
-        if (Input.GetKey(KeyCode.UpArrow))    pos.y = pos.y + 0.5f; 
-        if (Input.GetKey(KeyCode.DownArrow))  pos.y = pos.y - 0.5f;
-        if (Input.GetKey(KeyCode.LeftArrow))  pos.x = pos.x + 0.5f;
-        if (Input.GetKey(KeyCode.RightArrow)) pos.x = pos.x - 0.5f;
+    {
+        float horizontal = Input.GetAxis("JoyCon Horizontal L");
+        float vertical = Input.GetAxis("JoyCon Vertical L");
+    
+        Vector3 direction = new Vector3(-horizontal, vertical, 0).normalized;
 
-        transform.position = pos;
+        Debug.Log(direction);
+
+        pos += direction;
+
+        pos.x = Mathf.Clamp(pos.x, min.x, max.x);
+        pos.y = Mathf.Clamp(pos.y, min.y, max.y);
+
+        tran.position = pos;
     }
 }
