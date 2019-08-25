effa9e646803165a9c7eeecfbc2ee52cc41424bb
tatsuya-of-INM <is0514vf@ed.ritsumei.ac.jp>
2019-08-26 02:03:06 +0900

    的の衝突＆消去スクリプトの作成

---
+++ Assets/Scripts/BulletDestroy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider t)
+    {
+        Debug.Log(t.gameObject.name);
+        if (t.gameObject.name == "1p")
+        {
+            Debug.Log("Hit!");
+            Destroy(this.gameObject);
+        }
+    }
+}
